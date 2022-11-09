1ec8a9ebdd2b127fcd4f08cda1012f5d073a5fd9
adahan77 <adahan2894@gmail.com>
2022-11-09 19:44:54 +0600

    team

---
+++ 2slide.py
@@ -0,0 +1,51 @@
+
+print('problem 1')
+x = 17925<=(34**2)
+y = 17925<=(26*3)
+z = 17925<=(17*33)
+s = 17925<=(4394*4)
+print(x,y,z,s)
+
+print('problem 2')
+e = 7%3*4.8
+print(e)
+r = 5
+d = 5
+t = d==r
+print(t)
+g = 3
+u = 5
+i = g<u
+print(i)
+
+print('problem3')
+a = (8+2)*8**2-193432
+print(a)
+
+
+print('problem4')
+v = (17*3)<(12*5)
+q = (12**3)>(13*7)
+k = (4**5)==(512+512)
+print(v,q,k)
+
+
+print('problem5')
+print(-21//10)
+
+print('problem6')
+l = int(input('Введите прожитые годы: '))
+j = int(input('Введите сколько нужно: '))
+print((l/j)*100)
+
+print('problem7')
+o = 2005
+p = 2020
+print(p-o+2, p-2-o)
+
+
+print('problem8')
+print((25+75+10+95)/4)
+
+print('problem9')
+
